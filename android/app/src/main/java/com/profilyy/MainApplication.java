491c792f2a77dab21ecfc807bc5eb84fba968091
Vercillius <vercilliusjrmila@gmail.com>
2019-05-05 22:29:09 +0800

    unlinked slider

--- android/app/src/main/java/com/profilyy/MainApplication.java
+++ android/app/src/main/java/com/profilyy/MainApplication.java
@@ -3,7 +3,6 @@ package com.profilyy;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactSliderPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new AsyncStoragePackage()
--- components/EditPhoto.js
+++ components/EditPhoto.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Button, View, Text, Image, ImageEditor} from 'react-native';
+import {Button, View, TextInput, Image, ImageEditor} from 'react-native';
+import Slider from '@react-native-community/slider';
 
 
 class EditPhoto extends Component{
@@ -9,15 +10,15 @@ class EditPhoto extends Component{
 
       constructor(props){
           super(props);
-          this.state = {uri:this.props.navigation.getParam('uri')}
+          this.state = {uri:this.props.navigation.getParam('uri'), cropHeight: 300}
       }
 
       cropImage(){
           ImageEditor.cropImage(
               this.props.navigation.getParam('uri'),
               {
-                  offset:{ x: 10, y: 10 },
-                  size: {width: 50, height: 50}
+                  offset:{ x: 0, y: (this.props.navigation.getParam('height') - 300) / 2 },
+                  size: {width: this.props.navigation.getParam('width'), height: ((this.props.navigation.getParam('height') - 300) / 2) + 300}
               },
               (uri) =>{
                   console.log(uri);
@@ -30,15 +31,29 @@ class EditPhoto extends Component{
       }
     render(){
         return(
-            <View>
-                <Image
-                style={{
-                    height: this.props.navigation.getParam('height')
-                }}
-                source={{ uri: this.state.uri }}
-                />
+            <View style={{padding:15}}>
 
                 <View>
+                    <Image
+                    style={{
+                        //height: this.props.navigation.getParam('height'),
+            
+                        height: this.state.cropHeight
+
+                    }}
+                    source={{ uri: this.state.uri }}
+                    resizeMode="cover"
+                    />
+                </View>
+
+                <View>
+                    <Slider
+                        style={{width: 200, height: 40}}
+                        minimumValue={0}
+                        maximumValue={1}
+                        minimumTrackTintColor="#FFFFFF"
+                        maximumTrackTintColor="#000000"
+                    />
                     <Button title="Crop" onPress={()=>{this.cropImage()}} />
                 </View>
             </View>
