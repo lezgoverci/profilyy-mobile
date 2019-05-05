fdb1f16a5c3080236b77d51887e28ee28efc7f20
Vercillius <vercilliusjrmila@gmail.com>
2019-05-05 19:44:51 +0800

    load photos from camera roll

--- components/ProfilePhoto.js
+++ components/ProfilePhoto.js
@@ -1,15 +1,50 @@
 import React, {Component} from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, Text, ScrollView, CameraRoll, Image} from 'react-native';
 
 
 class ProfilePhoto extends Component{
     static navigationOptions = {
         headerTitle: 'Profile Photo',
       };
+
+      constructor(props){
+            super(props);
+            this.state = {photos:[]};
+      }
+
+      _handleButtonPress = () => {
+        CameraRoll.getPhotos({
+            first: 20,
+            assetType: 'Photos',
+          })
+          .then(r => {
+              console.log(r);
+            this.setState({ photos: r.edges });
+          })
+          .catch((err) => {
+             console.log(err);
+          });
+        }
+
     render(){
         return(
-            <View>
-                <Text>Profile Photo</Text>
+            <View style={{padding:15}}>
+                <Button title="Load Images" onPress={() =>{this._handleButtonPress()}} />
+                <ScrollView>
+                {this.state.photos.map((p, i) => {
+                return (
+                    <View key={i} style={{marginTop:15}}>
+                        <Image
+                        style={{
+                            flex:1,
+                            height:p.node.image.height
+                        }}
+                        source={{ uri: p.node.image.uri }}
+                        />
+                    </View>
+                );
+                })}
+                </ScrollView>
             </View>
         )
     }
