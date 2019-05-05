71f68df72b0c47accf733da343f640dead9c9951
Vercillius <vercilliusjrmila@gmail.com>
2019-05-05 21:10:05 +0800

    crop working

--- components/EditPhoto.js
+++ components/EditPhoto.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
-import {Button, View, Text, Image} from 'react-native';
+import {Button, View, Text, Image, ImageEditor} from 'react-native';
 
 
 class EditPhoto extends Component{
     static navigationOptions = {
         headerTitle: 'Edit Photo',
       };
+
+      constructor(props){
+          super(props);
+          this.state = {uri:this.props.navigation.getParam('uri')}
+      }
+
+      cropImage(){
+          ImageEditor.cropImage(
+              this.props.navigation.getParam('uri'),
+              {
+                  offset:{ x: 10, y: 10 },
+                  size: {width: 50, height: 50}
+              },
+              (uri) =>{
+                  console.log(uri);
+                  this.setState({uri:uri})
+              },
+              (err) => {
+                  console.log(err)
+              }
+          )
+      }
     render(){
         return(
             <View>
@@ -13,8 +35,12 @@ class EditPhoto extends Component{
                 style={{
                     height: this.props.navigation.getParam('height')
                 }}
-                source={{ uri: this.props.navigation.getParam('uri') }}
+                source={{ uri: this.state.uri }}
                 />
+
+                <View>
+                    <Button title="Crop" onPress={()=>{this.cropImage()}} />
+                </View>
             </View>
         )
     }
